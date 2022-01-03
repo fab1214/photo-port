8ba09c43ae0ca08ec4191a1d5152dc2dc6e2be0d
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-02 21:07:43 -0500

    add Nav test

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -8,20 +8,21 @@ import '@testing-library/jest-dom/extend-expect';
 
 import About from '..';
 
+//cleanup after each test to leave no lingering data/results
 afterEach(cleanup);
 
 //describe what we are testing...
 describe('About component',() => {
     //First test
-    //it or test works...
+    //'it()' or 'test()' works...
     test('renders', () => {
         render(<About></About>)
     })
     //Second test
     test('matches snapshot DOM node strcuture', () => {
-        //asFragment returns snapshot of the About component
+        //asFragment returns Jest snapshot of the About component
         const { asFragment } = render(<About></About>);
         //test and compare the expected snapshot (above) to actual snapshot rendered
         expect(asFragment()).toMatchSnapshot();
     })
-})
+});
---
+++ src/components/Nav/__test__/index.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+//'render' renders the component in simulated DOM, 'cleanup' function removes
+//components from DOM to prevent memory leaking, or collisions that could corrupt tests
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import Nav from '..';
+
+//cleanup after each test to leave no lingering data/results
+afterEach(cleanup);
+
+describe("Nav component", () => {
+    //baseline test
+    test('renders', () => {
+        render(<Nav></Nav>)
+    })
+    //snapshot test
+    test('matches snapshot', () => {
+        const { asFragment } = render(<Nav></Nav>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+    //snapshot test
+});
+
+//test if camera emoji is visible
+describe('emoji is visible', () => {
+    test('inserts emoji into h2', () => {
+        //Arrange
+        const { getByLabelText } = render(<Nav></Nav>);
+        //Assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    })
+});
+
+//test if Nav links are visible
+describe('nav links are visible', () => {
+    test('inserts text into the links', () => {
+        const { getByTestId } = render(<Nav></Nav>);
+
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About Me');
+    })
+});
