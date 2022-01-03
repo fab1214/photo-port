5ce827c5e8152e8994136683a597eda4414bb50f
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-02 20:45:04 -0500

    run test for About component

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//'render' renders the component in simulated DOM, 'cleanup' function removes
+//components from DOM to prevent memory leaking, or collisions that could corrupt tests
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup);
+
+//describe what we are testing...
+describe('About component',() => {
+    //First test
+    //it or test works...
+    test('renders', () => {
+        render(<About></About>)
+    })
+    //Second test
+    test('matches snapshot DOM node strcuture', () => {
+        //asFragment returns snapshot of the About component
+        const { asFragment } = render(<About></About>);
+        //test and compare the expected snapshot (above) to actual snapshot rendered
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
